4974264aea2a703f9758aacf35c0c379eee242c8
Letícia Cremasco <leticiacremasco@gmail.com>
2026-05-29 08:58:14 -0300

    Update pipeline_server.py
    ajuste versão do prompt

--- pipeline_server.py
+++ pipeline_server.py
@@ -737,7 +737,7 @@ async def _rodar_pipeline(analysis_id: str) -> None:
 
         # Buscar prompts do Supabase
         p_e1_rows = await _sb_get("prompts", "name=eq.PROMPT_00_PESQUISA_v6.0&select=content")
-        p_e2_rows = await _sb_get("prompts", "name=eq.PROMPT_01_EXTRACAO_v8.7&select=content")
+        p_e2_rows = await _sb_get("prompts", "name=eq.PROMPT_01_EXTRACAO_v8.9&select=content")
         p_e4_rows = await _sb_get("prompts", "name=eq.PROMPT_04_MEMORANDO_v2.7&select=content")
 
         if not p_e1_rows or not p_e2_rows or not p_e4_rows:
